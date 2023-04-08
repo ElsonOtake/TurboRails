4a7c54abfd0a1dd0de031fe4963a21733ad57de0
Elson Otake <elsonotake@yahoo.com.br>
2023-04-08 17:34:07 -0300

    Add attachment to email message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,7 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     @post = params[:post]
     @url  = 'http://example.com/login'
+    attachments['Puppy dog smiling.png'] = File.read("#{Rails.root}/app/assets/images/Puppy dog smiling.png")
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
