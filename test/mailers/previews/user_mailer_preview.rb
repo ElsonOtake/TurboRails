f78bcb105b3978865a840ea97a9a672d861a90c5
Elson Otake <elsonotake@yahoo.com.br>
2023-04-09 18:41:47 -0300

    Add email preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+    @user = User.first
+    @post = Post.first
+    UserMailer.with(user: @user, post: @post).welcome_email
+  end
 end
