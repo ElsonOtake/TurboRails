1fd475925cd497016505b462c8b42cec17939ff6
Elson Otake <elsonotake@yahoo.com.br>
2023-04-13 14:04:55 -0300

    Comment the attachments

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     @post = params[:post]
     @url  = 'http://example.com/login'
-    attachments.inline['Puppy dog smiling.png'] = File.read("#{Rails.root}/app/assets/images/Puppy dog smiling.png")
+    # attachments.inline['Puppy dog smiling.png'] = File.read("#{Rails.root}/app/assets/images/Puppy dog smiling.png")
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
